b4e783acff4468525d5ed9d707e8fd21605e86aa
CleoY <77142560+CleoY@users.noreply.github.com>
2021-10-01 23:45:01 -0700

    Updated magic square files

--- magicMain.c
+++ magicMain.c
@@ -8,55 +8,30 @@
 
 int main(){
 
-    int magicSquare2 [3][3] = { {4,9,2},
-                                {3,5,7},
-                                {8,1,6} };
+    int invalidMagicSquare[3][3] = { {8,2,5},
+                                     {2,5,8},
+                                     {5,8,2} };
 
-    int magicSquare3 [3][3] = { {8,2,5},
-                                {2,5,8},
-                                {5,8,2} };
+    //VALID square example
+    int validMagicSquare[3][3];
+    createSquare(validMagicSquare);
+    printSquare(validMagicSquare);
 
-    int magicSquare1[3][3];
-    createSquare(magicSquare1);
-    printSquare(magicSquare1);
-    
-    /*
-    int valid = checkValidSquare(magicSquare2) + checkDuplicates(magicSquare2);
-    printf("Magic square2 valid: %d\n", valid);
-    
-    valid = checkValidSquare(magicSquare3) + checkDuplicates(magicSquare3);
-    printf("Magic square3 valid: %d\n", valid);
-    */
+    int validSquareCheck = checkValidSquare(validMagicSquare) + checkDuplicates(validMagicSquare);
 
-/*
-    int dup = checkDuplicates(magicSquare3);
-    printf("magic3 does NOT have duplicates: %d\n", dup);
-*/
-    //createSquare();
-    //int magicSquare2 [3][3] = createSquare();
-
-    /*
-    int rowCheck = checkRow(magicSquare1);
-    printf("Row: %d\n",rowCheck);
-
-    int colCheck = checkCol(magicSquare1);
-    printf("Col: %d\n",colCheck);
-
-    int leftDiagonalCheck = checkLeftToRightDiagonal(magicSquare1);
-    printf("Left diagonal: %d\n",leftDiagonalCheck);
-
-    int rightDiagonalCheck = checkRightToLeftDiagonal(magicSquare1);
-    printf("Right diagonal: %d\n", rightDiagonalCheck);
-
-    int validSquareCheck = checkValidSquare(magicSquare1);
-    printf("Valid square: %d\n", validSquareCheck);
-
-    if (validSquareCheck == 1){
-        printf("Square is valid\n");
+    if (validSquareCheck == 2){
+        printf("First square is valid\n");
     } else{
-        printf("Square is NOT valid\n");
+        printf("First square is NOT valid\n");
     }
 
-    printSquare(magicSquare1);
-    */
+    //INvalid square example
+    printSquare(invalidMagicSquare);
+    validSquareCheck = checkValidSquare(invalidMagicSquare) + checkDuplicates(invalidMagicSquare);
+
+    if (validSquareCheck == 2){
+        printf("Second square is valid\n");
+    } else{
+        printf("Second square is NOT valid\n");
+    }
 }
--- magicSquare.c
+++ magicSquare.c
@@ -4,9 +4,6 @@
 #include "magicSquare.h"
 
 
-//DELETE COMMENTS BEFORE
-//CHANGE row CONDITION FROM 3 -> MORE FLEXIBLE
-
 int checkRow(int square[][3]);
 int checkCol(int square[][3]);
 int checkLeftToRightDiagonal(int square[][3]);
@@ -35,7 +32,7 @@ int createSquare(int square[][3]){
     }
 
     valid = checkValidSquare(square) + checkDuplicates(square);
-    printf("Valid: %d; ", valid);
+    //printf("Valid: %d; ", valid);
     if(valid != 2){
         printf("Attempt %d failed\n", squareCreationCount);
         squareCreationCount++;
@@ -49,24 +46,18 @@ int createSquare(int square[][3]){
 
 int checkDuplicates(int square[][3]){
     int temp1DArr[9];
-    
-    //printf("temp1D size: %lu\n", sizeof(square[0]));
-    
     int i = 0;
 
     for(int squareRow = 0; squareRow < 3; squareRow++){
         for(int squareCol = 0; squareCol < sizeof(square[0])/sizeof(int); squareCol++){
             temp1DArr[i] = square[squareRow][squareCol];
-            //printf("temp1D[%d] = %d\n", i, square[squareRow][squareCol]);
             i++;
         }
     }
     
     for(int size1D = 0; size1D < 9 - 1 ; size1D ++){
         for(int indexOfNextValue = size1D + 1; indexOfNextValue < 9; indexOfNextValue++){
-            //printf("Compare: %d @ %d  AND  %d @ %d\n", temp1DArr[size1D], size1D, temp1DArr[indexOfNextValue], indexOfNextValue);
             if(temp1DArr[size1D] == temp1DArr[indexOfNextValue]){
-                //printf("Duplicates found with %d and %d @ %d and %d\n", temp1DArr[size1D], temp1DArr[indexOfNextValue], size1D,indexOfNextValue);
                 return 0;
             }
         }
@@ -90,12 +81,9 @@ int checkRow(int square[][3]){
     int rowSum = 0;
     for(int row = 0; row < 3; row++){
         rowSum = 0;
-        //printf("Row size: %lu\n", sizeof(square[0]));
         for(int col = 0; col < sizeof(square[0])/sizeof(int); col++){
             rowSum += square[row][col];
-            //printf("Square[%d][%d]: %d, ", row, col, square[row][col]);
         }
-        //printf("Sum: %d\n", rowSum);
         if (rowSum != 15)
             return 0;
     }
@@ -133,11 +121,9 @@ int checkRightToLeftDiagonal(int square[][3]){
     int diagonalSum = 0;
     int row = 0;
     for(int col = sizeof(square[0])/sizeof(int) - 1; col >= 0; col--){
-        //printf("Square[%d][%d]: %d, ", row, col, square[row][col]);
         diagonalSum += square[row][col];
         row++;
     }
-    //printf("Sum: %d\n", diagonalSum);
     if(diagonalSum!=15){
         return 0;
     }
