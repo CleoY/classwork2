93e71bc0dc4035638c0ed2fd6d24f851d0b40f83
CleoY <77142560+CleoY@users.noreply.github.com>
2021-10-01 23:59:31 -0700

    Updated main

--- magicMain.c
+++ magicMain.c
@@ -11,6 +11,10 @@ int main(){
     int invalidMagicSquare[3][3] = { {8,2,5},
                                      {2,5,8},
                                      {5,8,2} };
+    
+    int anotherValidSquare [3][3] = { {4,9,2},
+                                 {3,5,7},
+                                 {8,1,6} };
 
     //VALID square example
     int validMagicSquare[3][3];
@@ -33,5 +37,15 @@ int main(){
         printf("Second square is valid\n");
     } else{
         printf("Second square is NOT valid\n");
+    }
+
+    //Another valid square
+    printSquare(anotherValidSquare);
+    validSquareCheck = checkValidSquare(anotherValidSquare) + checkDuplicates(anotherValidSquare);
+
+    if (validSquareCheck == 2){
+        printf("3rd square is valid\n");
+    } else{
+        printf("3rd square is NOT valid\n");
     }
 }
